6d30859359ced3e97a5eeb90665e6b9ec37af7b5
Ben Ramsey <ben@benramsey.com>
2022-09-09 11:19:04 -0500

    chore: initialize project using ramsey/php-library-starter-kit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Identifier\Test;
+
+use Identifier\Example;
+use Mockery\MockInterface;
+
+class ExampleTest extends TestCase
+{
+    public function testGreet(): void
+    {
+        /** @var Example & MockInterface $example */
+        $example = $this->mockery(Example::class);
+        $example->shouldReceive('greet')->passthru();
+
+        $this->assertSame('Hello, Friends!', $example->greet('Friends'));
+    }
+}
