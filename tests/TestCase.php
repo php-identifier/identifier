caf9cf41a8461b53b88898fc5ada6b9e72aaf753
Ben Ramsey <ben@benramsey.com>
2022-09-09 11:44:13 -0500

    chore: update dependencies and configs, as needed

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,13 +2,37 @@
 
 declare(strict_types=1);
 
-namespace Identifier\Test\Identifier;
+namespace Identifier\Test;
 
-use Ramsey\Dev\Tools\TestCase as BaseTestCase;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery\MockInterface;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 
 /**
  * A base test case for common test functionality
  */
-class TestCase extends BaseTestCase
+abstract class TestCase extends PHPUnitTestCase
 {
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * Configures and returns a mock object
+     *
+     * @param class-string<T> $class
+     * @param mixed ...$arguments
+     *
+     * @return T & MockInterface
+     *
+     * @template T
+     *
+     * phpcs:disable SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+     */
+    public function mockery(string $class, ...$arguments)
+    {
+        /** @var T & MockInterface $mock */
+        $mock = Mockery::mock($class, ...$arguments);
+
+        return $mock;
+    }
 }
