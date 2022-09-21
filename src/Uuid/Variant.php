074dced7cfea0cbce322af7894ea49fb9385fa22
Ben Ramsey <ben@benramsey.com>
2022-09-20 22:30:55 -0500

    chore: update docblocks

--- src/Uuid/Variant.php
+++ src/Uuid/Variant.php
@@ -8,12 +8,6 @@
  * (the "License"). You may not use this file except in
  * compliance with the License.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * permissions and limitations under the License.
- *
  * @copyright Copyright (c) Ben Ramsey <ben@benramsey.com>
  * @license https://opensource.org/licenses/MIT MIT License
  */
@@ -24,6 +18,21 @@ namespace Identifier\Uuid;
 
 /**
  * The variant number describes the layout of the UUID
+ *
+ *     Msb0  Msb1  Msb2  Description
+ *      0     x     x    Reserved, NCS backward compatibility.
+ *      1     0     x    The variant specified in this document.
+ *      1     1     0    Reserved, Microsoft Corporation backward
+ *                       compatibility
+ *      1     1     1    Reserved for future definition.
+ *
+ * In reading this table, we find that, if the first 3 bits of the variant field
+ * are all 1s (i.e., the decimal value 7), then the variant is reserved for
+ * future definition. If the first three bits are two 1s followed by a 0 (i.e.,
+ * the decimal value 6), then the variant is reserved for Microsoft
+ * Corporation. If the first two bits are a 1 and 0 (i.e., the decimal value
+ * 2), then the variant is for RFC 4122. Finally, if the first bit is 0,
+ * then it's reserved for NCS, for backward compatibility.
  *
  * @link https://datatracker.ietf.org/doc/html/rfc4122#section-4.1.1 RFC 4122, § 4.1.1
  */
